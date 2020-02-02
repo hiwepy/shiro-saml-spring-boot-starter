7011063b6a4fa4c14573fed37942eef95f78d8bc
wandl <hnxyhcwdl1003@163.com>
2020-02-02 11:47:53 +0800

    优化

--- src/main/java/org/apache/shiro/spring/boot/saml/authc/SamlLogoutFilter.java
+++ src/main/java/org/apache/shiro/spring/boot/saml/authc/SamlLogoutFilter.java
@@ -13,7 +13,7 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  */
-package org.apache.shiro.spring.boot.saml;
+package org.apache.shiro.spring.boot.saml.authc;
 
 import org.apache.shiro.biz.web.filter.authc.AbstractLogoutFilter;
 
