ccf74afc5f80d754c2e84f30afe33bb4687a1ccd
wandl-6A72h <hiwepy@gmail.com>
2026-08-09 00:07:06 +0800

    chore: propagate pom standardization, tests, javadoc and @author fix from 4.1.x
    - Add jacoco-maven-plugin 0.8.15 with 90% coverage gate
    - Add central-publishing-maven-plugin 0.11.0
    - Update surefire (enable tests, argLine for JaCoCo, *_Test.java pattern)
    - Fix SCM URLs
    - Copy test files from 4.1.x branch
    - Copy Javadoc improvements from 4.1.x branch
    - Standardize @author to [@Loong Wan](https://github.com/loong10k)
    - Propagated to 3.0.x branch

--- src/main/java/org/apache/shiro/spring/boot/saml/authz/SamlAuthorizationFilter.java
+++ src/main/java/org/apache/shiro/spring/boot/saml/authz/SamlAuthorizationFilter.java
@@ -26,7 +26,7 @@ import com.alibaba.fastjson.JSONObject;
 
 /**
  * SAML 1.x 授权 (authorization)过滤器
- * @author ： <a href="https://github.com/hiwepy">hiwepy</a>
+ * @author [@Loong Wan](https://github.com/loong10k)
  */
 public class SamlAuthorizationFilter extends AbstracAuthorizationFilter {
 
